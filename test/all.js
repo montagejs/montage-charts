583b4545a1307d374a1ff7d95eaaef3b8155a52c
Corentin Debost <codebost@gmail.com>
2020-04-24 10:42:46 -0700

    Test Harness

---
+++ test/all.js
@@ -0,0 +1,6 @@
+module.exports = require("montage-testing").run(require, [
+]).then(function () {
+    console.log('montage-testing', 'End');
+}, function (err) {
+    console.log('montage-testing', 'Fail', err, err.stack);
+});
